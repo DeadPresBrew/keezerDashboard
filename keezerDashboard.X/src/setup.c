8e50a253e85b0f455c362f1fa1e56d17443bd988
erdmannicus <jerdmann@GEN1206-12.generac.com>
2015-10-15 17:27:59 -0500

    continue setup activities

--- keezerDashboard.X/src/setup.c
+++ keezerDashboard.X/src/setup.c
@@ -3,6 +3,9 @@
  * Author: jerdmann
  *
  * Created on October 14, 2015, 3:52 PM
+ *
+ *
+ * target MCU is PIC24FJ64GA002
  */
 
 // Internal 8MHz oscillator + PLL.
@@ -26,11 +29,17 @@ void
 setup (void)
 {
     //gpio
-    TRISAbits = 0x3;
-    TRISBbits = 0xfee0;
     AD1PCFG = 0x1FFC;
-    AD1CON2bits.VCFG = 0x0;
-    AD1CON3bits.ADCS = 0x7;
+    TRISA = 0x3;
+    TRISB = 0xfee0;
+    //TODO(josh): setup analog inputs... maybe
+//    AD1CON2bits.VCFG = 0x0;
+//    AD1CON3bits.ADCS = 0x7;
+
+    //peripheral pin select
+    RPINR18bits.U1RXR = 24; //assign U1RX to pin 24
+    RPOR0bits.RP0R = 7;     //assign SDO1 to RP0
+    RPOR0bits.RP1R = 8;     //assign SCK1 to RP1
 
     //spi
 
@@ -39,4 +48,7 @@ setup (void)
     //uart
 
     //timers
+
+    //interrupts
+
 }
